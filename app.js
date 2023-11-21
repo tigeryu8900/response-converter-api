b02f7a17b1e871e60a04804e388d1552cfbdf894
tigeryu8900 <tigeryu8900@gmail.com>
2023-11-21 10:00:45 -0800

    don't terminate on error

--- app.js
+++ app.js
@@ -5,36 +5,46 @@ const app = express();
 const regexpParser = /^(?<separator>.)(?<body>.*)\1(?<flags>\w*)$/;
 
 app.get('/', async (req, res) => {
-    let response = await fetch(req.query.url, {
-        ...req.query,
-        headers: req.query.headers ? JSON.parse(req.query.headers) : undefined
-    });
-    let text = await response.text();
-    req.fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-    if (req.query.replacements) {
-        let replacements = JSON.parse(req.query.replacements);
-        for (let find in replacements) {
-            let replace = replacements[find];
-            if (req.query.regex === "true") {
-                let match = find.match(regexpParser);
-                find = new RegExp(match.groups.body, match.groups.flags);
+    try {
+        let response = await fetch(req.query.url, {
+            ...req.query,
+            headers: req.query.headers ? JSON.parse(req.query.headers) : undefined
+        });
+        let text = await response.text();
+        req.fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+        if (req.query.replacements) {
+            let replacements = JSON.parse(req.query.replacements);
+            for (let find in replacements) {
+                let replace = replacements[find];
+                if (req.query.regex === "true") {
+                    let match = find.match(regexpParser);
+                    find = new RegExp(match.groups.body, match.groups.flags);
+                }
+                text = text.replace(find, replace
+                    .replace(/(?<=(?<!\$)(?:\$\$)*)\$<(\w+)>/g, (match, $1) => {
+                        return String(req[$1])
+                            .replaceAll("$", "$$$$")
+                    }));
             }
-            text = text.replace(find, replace
-                .replace(/(?<=(?<!\$)(?:\$\$)*)\$<(\w+)>/g, (match, $1) => {
-                    return String(req[$1])
-                        .replaceAll("$", "$$$$")
-                }));
+        }
+        res.status(response.status);
+        response.headers.forEach((value, key) => {
+            try {
+                res.set(key, value);
+            } catch (e) {
+                console.error(e);
+            }
+        });
+        res.send(text);
+    } catch (e) {
+        console.error(e);
+        res.status(500);
+        if (e instanceof Error) {
+            res.send(e.stack);
+        } else {
+            res.send(e?.message || e?.name || e);
         }
     }
-    res.status(response.status);
-    response.headers.forEach((value, key) => {
-        try {
-            res.set(key, value);
-        } catch (e) {
-            console.error(e);
-        }
-    });
-    res.send(text);
 });
 
 // Start the server
