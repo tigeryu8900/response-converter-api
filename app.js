7de6462e262e53517bba59bdb7c485042faccb6e
tigeryu8900 <tigeryu8900@gmail.com>
2023-11-22 10:08:23 -0800

    redo implementation

--- app.js
+++ app.js
@@ -16,41 +16,92 @@ app.use (function(req, res, next) {
 
 const regexpParser = /^(?<separator>.)(?<body>.*)\1(?<flags>\w*)$/;
 
-app.all('/', async (req, res) => {
+//'/:options/:url([\\w\\W]*)'
+app.all("*", async (req, res) => {
     try {
-        let response = await fetch(req.query.url, {
-            method: req.method,
-            ...req.query,
-            headers: req.headers,
-            body: req.body
+        let match = req.path.match(/^\/(?<options>[^\/]+)?\/(?<url>.*)$/)?.groups;
+        let { options, url } = await new Promise((resolve, reject) => {
+            if (match) {
+                try {
+                    let options = JSON.parse(match.options ?? "{}");
+                    let url = new URL(match.url);
+                    if (!/[.:]/.test(url.host)) {
+                        let referer = req.headers.referer ?? req.headers.origin;
+                        if (referer) {
+                            referer = new URL(referer);
+                            if (["localhost:8080", "response-converter-api.onrender.com"].includes(referer.host)) {
+                                referer = new URL(referer.pathname.replace(/^\/.*?\//, ""));
+                            }
+                        }
+                        url = new URL(url.host + url.pathname, referer);
+                    }
+                    for (let [key, value] of Object.entries(req.query)) {
+                        url.searchParams.set(key, value);
+                    }
+                    resolve({ options, url });
+                } catch (e) {
+                    reject();
+                }
+            } else {
+                reject();
+            }
+        }).catch(() => {
+            let referer = req.headers.referer ?? req.headers.origin;
+            if (referer) {
+                referer = new URL(referer);
+                if (["localhost:8080", "response-converter-api.onrender.com"].includes(referer.host)) {
+                    referer = new URL(referer.pathname.replace(/^\/.*?\//, ""));
+                }
+            }
+            let url = new URL(req.path, referer);
+            return { options: {}, url };
         });
-        let text = await response.text();
-        req.fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-        if (req.query.replacements) {
-            let replacements = JSON.parse(req.query.replacements);
-            for (let find in replacements) {
-                let replace = replacements[find];
-                if (req.query.regex === "true") {
+        let response = await fetch(url, {
+            method: options.method ?? req.method,
+            headers: {
+                ...req.headers,
+                ...options["request-headers"] ?? {}
+            },
+            credentials: options.credentials ?? req.credentials,
+            body: options.body ?? req.body,
+            cache: options.cache ?? req.cache,
+            redirect: options.redirect ?? req.redirect,
+            referrer: options.referrer ?? req.referrer,
+            referrerPolicy: options.referrerPolicy ?? req.referrerPolicy,
+            integrity: options.integrity ?? req.integrity,
+        });
+        let data;
+        if (options.replacements &&
+            ["text", "application"].includes(response.headers.get("content-type")?.split("/")[0] ?? "text")) {
+            data = await response.text();
+            for (let [find, replace] in Object.entries(options.replacements)) {
+                if (options.regex) {
                     let match = find.match(regexpParser);
                     find = new RegExp(match.groups.body, match.groups.flags);
                 }
-                text = text.replace(find, replace
+                data = data.replace(find, replace
                     .replace(/(?<=(?<!\$)(?:\$\$)*)\$<(\w+)>/g, (match, $1) => {
                         return String(req[$1])
                             .replaceAll("$", "$$$$")
                     }));
             }
+        } else {
+            data = await response.arrayBuffer();
         }
         res.status(response.status);
         response.headers.forEach((value, key) => {
             try {
-                res.set(key, value);
+                if (![
+                    "content-encoding"
+                ].includes(key.toLowerCase())) {
+                    res.set(key, value);
+                }
             } catch (e) {
                 console.error(e);
             }
         });
-        if (req.query.headers) {
-            for (let [key, value] of Object.entries(JSON.parse(req.query.headers))) {
+        if (options["response-headers"]) {
+            for (let [key, value] of Object.entries(options["response-headers"])) {
                 try {
                     res.set(key, String(value));
                 } catch (e) {
@@ -58,9 +109,9 @@ app.all('/', async (req, res) => {
                 }
             }
         }
-        res.send(text);
+        res.send(Buffer.from(data));
     } catch (e) {
-        console.error(e);
+        console.error(req.path, e);
         res.status(500);
         if (e instanceof Error) {
             res.send(e.stack);
